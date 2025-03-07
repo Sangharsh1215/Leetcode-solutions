783f4651f8153f92cb523f81f23e30a3c7515c62
Sangharsh <135854647+Sangharsh1215@users.noreply.github.com>
2025-03-07 20:22:45 +0530

    Create file

---
+++ 148-SortList/148-SortList.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def sortList(self, head):
+        arr = []
+        temp = head
+
+        while temp:
+            arr.append(temp.val)
+            temp = temp.next
+
+        arr.sort()
+        temp = head
+
+        for i in range(len(arr)):
+            temp.val = arr[i]
+            temp = temp.next
+
+        return head
+            
+        
