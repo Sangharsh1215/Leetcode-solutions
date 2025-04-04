742a9365417d76db786904968ad5cf46b4c15a0e
Sangharsh <135854647+Sangharsh1215@users.noreply.github.com>
2025-04-04 18:43:04 +0530

    Create file

---
+++ 42-TrappingRainWater/42-TrappingRainWater.py
@@ -0,0 +1,32 @@
+# Last updated: 4/4/2025, 6:43:03 PM
+class Solution(object):
+    def trap(self, height):
+        x = max(height)
+        i = height.index(x)
+        maxi = 0
+        wb = 0
+        res = 0
+        n = len(height)
+
+        for j in range(1,i+1):
+            if height[j] >= height[maxi]:
+                res += wb
+                wb = 0
+                maxi = j
+            wb += (height[maxi] - height[j])
+
+        maxi = n-1
+        wb = 0
+
+
+        for k in range(n-2,i-1,-1):
+            if height[k] >= height[maxi]:
+                res += wb
+                wb = 0
+                maxi = k
+            wb += (height[maxi] - height[k])
+
+        return res 
+
+
+        
