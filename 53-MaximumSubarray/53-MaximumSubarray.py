646f4607b460ef68f135c2c62b5fbba859e644ea
Sangharsh <135854647+Sangharsh1215@users.noreply.github.com>
2025-07-28 16:48:26 +0530

    File updated at 7/28/2025, 4:48:27 PM

--- 53-MaximumSubarray/53-MaximumSubarray.py
+++ 53-MaximumSubarray/53-MaximumSubarray.py
@@ -1,18 +1,15 @@
-# Last updated: 7/28/2025, 4:47:44 PM
+# Last updated: 7/28/2025, 4:48:27 PM
 class Solution(object):
     def maxSubArray(self, nums):
         n = len(nums)
-        sum = 0 
-        max = float('-inf')
+        sum = 0
+        maxi = float('-inf')
+
         for i in range(n):
             sum = sum + nums[i]
-
-            if sum > max:
-                max = sum
+            if sum > maxi:
+                maxi = sum
             if sum < 0:
                 sum = 0
-
-
-        
-        return max
+        return maxi
         
